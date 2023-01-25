11e7793f4f3b22f6b68f4f81a5ef17f7554fd518
Suwandi Kurniawan <suwandikurniawan89@gmail.com>
2023-01-25 15:14:35 +0700

    First step : create static component

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,17 @@
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
+
+const ExpenseItem = (data) => {
+    return (
+        <Card className="expense-item">
+            <ExpenseDate date={data.date}/>
+            <div className="expense-item__description">
+                <h2>{data.title}</h2>
+                <div className="expense-item__price">${data.amount}</div>
+            </div>
+        </Card>
+    )
+}
+
+export default ExpenseItem;
